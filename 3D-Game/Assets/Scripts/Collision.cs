9e39e8cd5e0fbd14fdbad76d1006a715351a90b7
Marcos Ayon <marcosayon@email.arizona.edu>
2018-10-30 22:52:20 -0700

    Finished Game

--- 3D-Game/Assets/Scripts/Collision.cs
+++ 3D-Game/Assets/Scripts/Collision.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour {
     
     public GameObject bad;
     public GameObject portal;
+
+    public AudioSource tires;
+    public AudioSource explosion;
+    public AudioSource success;
 
 
 	// Use this for initialization
@@ -15,15 +20,21 @@ public class Collision : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.anyKeyDown){
+            tires.Play();
+        }
 	}
 
     public void OnParticleCollision(GameObject other)
     {
         if(other == bad){
-            Debug.Log("WORKS");
+            SceneManager.LoadScene("EndScreen");
+            explosion.Play();
         }
 
         if(other == portal){
+            Points.pointVal += 1;
+            success.Play();
             portal.transform.position = new Vector3(Random.Range(-33.0f, 36.0f), -4.0f, Random.Range(-87.0f, 83.0f));
         }
     }
---
+++ 3D-Game/Assets/Scripts/Points.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour {
+
+    public static int pointVal = 0;
+    Text points;
+
+	// Use this for initialization
+	void Start () {
+        points = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        points.text = "Score : " + pointVal;
+	}
+}
