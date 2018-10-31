f03c57a802523c0d653b991e615306180baa9951
Marcos Ayon <marcosayon@email.arizona.edu>
2018-10-30 17:14:26 -0700

    fixed skybox

--- 3D-Game/Assets/Scripts/CarController.cs
+++ 3D-Game/Assets/Scripts/CarController.cs
@@ -19,7 +19,7 @@ public class CarController : MonoBehaviour
 	
 	void Update() {
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 30.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 60.0f;
 
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
