05008106cb0661dd6d5e55bbc8c0185039dba83e
Marcos Ayon <marcosayon@email.arizona.edu>
2018-10-30 20:36:02 -0700

    Added moving portals

---
+++ 3D-Game/Assets/Scripts/Collision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+    
+    public GameObject bad;
+    public GameObject portal;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void OnParticleCollision(GameObject other)
+    {
+        if(other == bad){
+            Debug.Log("WORKS");
+        }
+
+        if(other == portal){
+            portal.transform.position = new Vector3(Random.Range(-33.0f, 36.0f), -4.0f, Random.Range(-87.0f, 83.0f));
+        }
+    }
+}
